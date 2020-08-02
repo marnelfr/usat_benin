94384e616570381f544b24b8341bc9fc7e9e012a
Marnel GNACADJA <gmlginolias@gmail.com>
2020-08-02 15:32:22 +0100

    Adding webpack-encore

--- src/Controller/GuestController.php
+++ src/Controller/GuestController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Vehicule;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
