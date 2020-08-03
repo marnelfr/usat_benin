ea8c3cd004c0da1154c1b0845a0814c770af164a
Marnel <gmlginolias@gmail.com>
2020-08-03 19:59:29 +0100

    Setting up the authenticator form. Starting the registration form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,15 +2,35 @@
 
 namespace App\Entity;
 
+use App\Repository\ProfilRepository;
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+
+/*
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
+ */
 
 /**
+ * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorColumn(name="user_type", type="string")
+ * @ORM\DiscriminatorMap({
+ *   "User" = "User",
+ *   "Agent" = "Agent",
+ *   "Importer" = "Importer",
+ *   "Remover" = "Remover",
+ *   "Manager" = "Manager"
+ * })
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -18,6 +38,22 @@ class User
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -75,15 +111,104 @@ class User
      */
     private $fleets;
 
-    public function __construct()
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
+
+
+
+
+
+    public function __construct($p)
     {
+        $this->name = 'Admin';
+        $this->lastName = 'Admin';
+        $this->address = '0:0';
+        $this->createdAt = new \DateTime();
+        $this->lastConnection = new \DateTime();
+        $this->email = 'marnognac@gmail.com';
+        $this->phone ='';
+        $this->profil = $p;
+        $this->roles[] = 'ROLE_ADMIN';
+        $this->status= 0;
+
         $this->processings = new ArrayCollection();
         $this->fleets = new ArrayCollection();
     }
 
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getName(): ?string
@@ -105,7 +230,7 @@ class User
 
     public function setLastName(string $lastName): self
     {
-        $this->lastName = $lastName;
+        $this->lastName = mb_strtoupper($lastName);
 
         return $this;
     }
@@ -252,6 +377,23 @@ class User
                 $fleet->setUser(null);
             }
         }
+
+        return $this;
+    }
+    
+    public function getFullname(): ?string
+    {
+        return $this->getName() . ' ' . $this->getLastName();
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
