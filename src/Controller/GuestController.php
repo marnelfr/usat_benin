43a80a72c8240049a21503bb5213c6149c83f059
Marnel <gmlginolias@gmail.com>
2020-08-03 21:35:30 +0100

    Login form done && started with registration form

--- src/Controller/GuestController.php
+++ src/Controller/GuestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GuestController extends AbstractController
@@ -13,8 +14,17 @@ class GuestController extends AbstractController
      *
      * @Route("/", name="home_page")
      */
-    public function index(EntityManagerInterface $em)
+    public function index(EntityManagerInterface $em, Request $request)
     {
-        return $this->render('guest/home/home.html.twig');
+//        dd($request->request->all(), $request->query->all(), $request->get('last_username'), $request->get('error'));
+
+        $login = $request->get('login');
+
+        $data = [
+            'login_form'        => $login === null ? 0 : $login,
+            'registration'      => isset($request->getSession()->getFlashBag()->get('registration')[0])
+        ];
+        dump($data);
+        return $this->render('guest/home/home.html.twig', $data);
     }
 }
