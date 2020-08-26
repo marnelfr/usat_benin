7145a7163da1973e10368df2acdb122686da7e03
Marnel <gmlginolias@gmail.com>
2020-08-26 09:29:41 +0100

    Fin comment de tout ce qui a été fait hier

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -7,13 +7,12 @@ import 'bootstrap'
 
 import '../js/adminlte.min'
 import '../js/demo'
-import Button from './import/button'
-import Modal from './import/modal'
 import u from './import/utility'
 
 
 $(function () {
   setTimeout(function () {
+    //Permet d'afficher les alerts au chargement de la page
     $('.alert').each(function () {
       const alert = $(this)
       u.notif(alert.val(), alert.attr('data-type'))
@@ -21,64 +20,71 @@ $(function () {
   }, 2000)
 })
 
-
-
-
 $(function () {
-    let changer = document.querySelector('#e84nel915737a83ea2a59627de15bc521')
-    if (!!changer) {
-      import('../../public/js/fos_js_routes.json').then(({default: routes}) => {
-        import('../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js').then(({default: Routing}) => {
-          Routing.setRoutingData(routes);
-          $.get(Routing.generate('pcn')).then(function (data) {
-            if (data.typeMessage) {
-              const modal = new Modal(true)
-              modal.setContent(data.view)
-              u.notif("Merci de personnalisez votre code d'accès <br>C'est important de garder son code secret.", 'info')
-              modal.show(function () {
-                let modalBtnSaver = new Button('#change-password-saver')
-                modalBtnSaver.click(function () {
-                  let $form = modalBtnSaver.getForm()
-                  let formData = new FormData($form[0])
-                  $.ajax({
-                    url: $form.attr('action'),
-                    type: 'POST',
-                    data: formData,
-                    processData: false,
-                    contentType: false,
-                    success: function (data) {
-                      //Si typeMessage n'est pas défini, alors c'est pas moi-même, j'ai fait le retour
-                      if (!!data.typeMessage) {
-                        //C'est qu'il y a une erreur par rapport à la validation du formulaire
-                        if (data.typeMessage === 'form') {
-                          //On affiche donc le nouveau formulaire reçu
-                          let div = $form.closest('div')
-                          div.html(data.form)
-                          u.notif('Les codes doivent être les mêmes et comporter au moins 6 charatères.', 'info')
-                          modalBtnSaver.reset()
-                        } else
-                        //Un vehicule a été trouvé, on affiche donc ses informations dans un modal
-                        if (data.typeMessage === 'success') {
-                          window.location = data.link
+  //Si l'input ayant pour id, ce token est présent dans menu_items.html.twig,
+  // c'est que l'utilisateur doit changer son mot de passe
+  let changer = document.querySelector('#e84nel915737a83ea2a59627de15bc521')
+  if (!!changer) {
+    import('../../public/js/fos_js_routes.json').then(({default: routes}) => {
+      import('../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js').then(({default: Routing}) => {
+        import('./import/button').then(({default: Button}) => {
+          import('./import/modal').then(({default: Modal}) => {
+            Routing.setRoutingData(routes);
+            //On charge le contenu du formulaire
+            $.get(Routing.generate('pcn')).then(function (data) {
+              if (data.typeMessage) {
+                const modal = new Modal(true)
+                modal.setContent(data.view)
+                //On informe l'utilisateur de ce qui se passe
+                u.notif("Merci de personnalisez votre code d'accès <br>C'est important de garder son code secret.", 'info')
+                //On affiche le formulaire
+                modal.show(function () {
+                  let modalBtnSaver = new Button('#change-password-saver')
+                  //Le formulaire sera soumit par ajax
+                  modalBtnSaver.click(function () {
+                    let $form = modalBtnSaver.getForm()
+                    let formData = new FormData($form[0])
+                    $.ajax({
+                      url: $form.attr('action'),
+                      type: 'POST',
+                      data: formData,
+                      processData: false,
+                      contentType: false,
+                      success: function (data) {
+                        //Si typeMessage n'est pas défini, alors c'est pas moi-même, j'ai fait le retour
+                        if (!!data.typeMessage) {
+                          //C'est qu'il y a une erreur par rapport à la validation du formulaire
+                          if (data.typeMessage === 'form') {
+                            //On affiche donc le nouveau formulaire reçu
+                            let div = $form.closest('div')
+                            div.html(data.form)
+                            u.notif('Les codes doivent être les mêmes et comporter au moins 6 charatères.', 'info')
+                            modalBtnSaver.reset()
+                          } else
+                          //On redirige l'utilisateur vers link si le mot de passe à été modifié avec succès
+                          if (data.typeMessage === 'success') {
+                            window.location = data.link
+                          } else {
+                            u.notif('Echec de chargement...', 'warning')
+                          }
                         } else {
-                          u.notif('Echec de chargement...', 'warning')
+                          u.notif('Echec de chargement... Veuillez vérifier votre connexion internet et réessayer.', 'danger')
                         }
-                      } else {
-                        u.notif('Echec de chargement... Veuillez vérifier votre connexion internet et réessayer.', 'danger')
+                      },
+                      error: function () {
+                        modalBtnSaver.reset()
+                        u.notif("Echec de chargement. Veuillez vérifier votre connexion internet et réessayer", 'danger');
                       }
-                    },
-                    error: function () {
-                      modalBtnSaver.reset()
-                      u.notif("Echec de chargement. Veuillez vérifier votre connexion internet et réessayer", 'danger');
-                    }
+                    })
                   })
                 })
-              })
-            }else{
-              u.notif('danger', 'Erreur de chargement..')
-            }
+              } else {
+                u.notif('Erreur de chargement. Veuillez vérifier votre connexion internet et réessayer', 'danger')
+              }
+            })
           })
         })
       })
-    }
+    })
+  }
 })
--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -16,18 +16,23 @@ class MenuController extends AbstractController
 
             $user = $this->getUser();
             $role = $user->getRoles()[0];
+            //Un staff_admin n'est d'un staff avec un tout peut privilège dessus: la gestion des utilisateurs
+            //Donc il n'a pas de lien personnel
             if ($role === 'ROLE_STAFF_ADMIN') {
                 $role = 'ROLE_STAFF';
             }
             $role = strtolower($role);
             $role = str_replace('role_', '', $role);
 
+            //On construit le lien du tableau de bord
             $data = [
                 'dashboard_path' => 'actors_' . $role . '_dashboard'
             ];
 
             $profil = $user->getProfil()->getSlug();
             $data['change_password'] = false;
+            //S'il s'agit pas d'un agent ni d'un manager, on ne vérifie en reéalité par le mail
+            //La vérification de l'email se substitue ici à la personnalisation du mot de passe qui est obligatoire
             if ($profil !== 'agent' && $profil !== 'manager' && !$user->getIsVerified()) {
                 $data['change_password'] = true;
             }
