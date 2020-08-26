421855c281337d1e7ea18c907a9dd85ada093797
Marnel <gmlginolias@gmail.com>
2020-08-26 16:53:02 +0100

    user edit à revoir

--- assets/js/user/user.js
+++ assets/js/user/user.js
@@ -3,4 +3,6 @@ import Button from '../import/button'
 $(function () {
   let btn = new Button('.btn-user-saver')
   btn.loadOnClick(true)
+
+  // TODO: remove() le champs du password sur le formulaire d'édition de la personne
 })
