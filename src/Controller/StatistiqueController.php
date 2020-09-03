022292a46da47b173e1077c1f850b24095bba4a5
Marnel Fresh'eur <gmlginolias@gmail.com>
2020-09-03 08:08:01 +0100

    Fin page d'accueil & debut statistique

--- src/Controller/StatistiqueController.php
+++ src/Controller/StatistiqueController.php
@@ -3,17 +3,40 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StatistiqueController extends AbstractController
 {
     /**
-     * @Route("/statistique", name="statistique")
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $form = $this->createFormBuilder()
+            ->add('from', DateType::class, [
+                'mapped' => false,
+                'label' => 'Date début',
+                'placeholder' => 'Renseigner une date début',
+                'widget' => 'single_text',
+                'data' => new \DateTime()
+            ])
+            ->add('to', DateType::class, [
+                'mapped' => false,
+                'label' => 'Date fin',
+                'placeholder' => 'Renseignez une date fin',
+                'widget' => 'single_text',
+                'data' => new \DateTime()
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
         return $this->render('statistique/index.html.twig', [
-            'controller_name' => 'StatistiqueController',
+            'form' => $form->createView()
         ]);
     }
 }
