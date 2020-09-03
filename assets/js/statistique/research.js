45ff1d096e6db201d794de4140f3e3ffe8502cc5
Marnel <gmlginolias@gmail.com>
2020-09-03 19:36:32 +0100

    Debut de la mise en place des statistics

--- assets/js/statistique/research.js
+++ assets/js/statistique/research.js
@@ -18,18 +18,22 @@ export default class Research{
     Routing.setRoutingData(routes);
 
     //La première exécution au chargement de la page
-    this.search()
+    this.search(true)
 
     //A chaque fois qu'on clique sur le bouton de chargement
     this.btn.click(this.search)
   }
 
-  search() {
+  search(initialLoad = false) {
     const dateDebut = this.debut.val()
     const dateFin = this.fin.val()
-    $.get(Routing.generate('statistic_load', {debut: dateDebut, fin: dateFin})).then(function (data) {
+    $.get(Routing.generate('statistic_load', {debut: dateDebut, fin: dateFin, initial: initialLoad})).then(function (data) {
       if (data.typeMessage) {
-        this.container.html(data.view)
+        if (data.typeMessage === 'warning') {
+          u.notif(data.message, data.typeMessage)
+        } else {
+          this.container.html(data.view)
+        }
       }else{
         u.notif('Erreur de chargement des statistiques', 'danger')
       }
--- src/Controller/StatisticController.php
+++ src/Controller/StatisticController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -50,6 +52,48 @@ class StatisticController extends AbstractController
      * @param Request $request
      */
     public function load(Request $request) {
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if ($request->isXmlHttpRequest()) {
+            $debut = new \DateTime($request->get('debut'));
+            $fin = new \DateTime($request->get('fin'));
+            if (!$debut || !$fin) {
+                return new JsonResponse([
+                    'typeMessage' => 'warning',
+                    'message' => 'Veuillez renseigner une date de début et une date fin'
+                ]);
+            }
+            $statistics = $this->getStatistics($debut, $fin);
+            return new JsonResponse([
+                'typeMessage' => 'success',
+                'view' => $this->renderView('statistic/show.html.twig', $statistics)
+            ]);
+        }
+        return new Response('Accès interdit');
+    }
+
+    private function getStatistics($debut, $fin)
+    {
+        /**
+         * nombre de transfert qui à été
+         *      demander
+         *      dont le traitement est encours
+         *      dont le traitement a été aprouvé
+         *      rejeter
+         * nombre d'enlevement qui a été
+         *      demander
+         *      dont le traitment est encours
+         *      dont le traitement est approuvé
+         *      rejeter
+         * nombre de gestionnaire qu'on a
+         * nombre de commissionnaire qu'on a
+         * nombre importer
+         * nombre de verification de fiche imprimer qui a été effectué
+         * nombre de vehicule qui a eté enregistré au total
+         */
+
+
 
     }
 }
