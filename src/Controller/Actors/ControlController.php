4342a7670d9f08bd2a39b725f6a9e90021146ded
Marnel <gmlginolias@gmail.com>
2020-09-10 16:17:47 +0100

    Fin tableau de bord des contrôleurs

--- src/Controller/Actors/ControlController.php
+++ src/Controller/Actors/ControlController.php
@@ -19,13 +19,17 @@ class ControlController extends AbstractController
      * Le tableau de bord des contrôleurs de la plateforme
      *
      * @Route("/actors/control", name="actors_control_dashboard")
+     * @param StaffController $staffController
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(StaffController $staffController)
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        return $this->render('actors/control/index.html.twig', [
-            'controller_name' => 'ControlController',
-        ]);
+        $data = $staffController->getMiniStatistics();
+        $data['control'] = true;
+        $data['staff'] = false;
+        return $this->render('actors/staff/index.html.twig', $data);
     }
 }
